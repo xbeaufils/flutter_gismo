4e1f8b03fc4962a8be963d0a60def405470cca5c
xbeau <xbeaufils@gmail.com>
2021-02-12 12:38:03 +0100

    Bluetooth

---
+++ lib/model/BuetoothModel.dart
@@ -0,0 +1,21 @@
+class BluetoothState {
+  String _status;
+  String _data;
+
+  BluetoothState.fromResult(result) {
+    _status = result["status"];
+    _data = result["data"];
+  }
+
+  String get data => _data;
+
+  set data(String value) {
+    _data = value;
+  }
+
+  String get status => _status;
+
+  set status(String value) {
+    _status = value;
+  }
+}
