d943ef84ad683d63538f02408744390d92b5cc26
xbeau <xbeaufils@gmail.com>
2021-06-04 10:30:41 +0200

    Flutter 2

--- lib/bloc/AbstractDataProvider.dart
+++ lib/bloc/AbstractDataProvider.dart
@@ -14,15 +14,15 @@ abstract class DataProvider {
   GismoBloc get bloc => _bloc;
 
   DataProvider(this._bloc);
-  String get cheptel => this._bloc.user.cheptel;
-  String get token => this._bloc.user.token;
+  String ? get cheptel => this._bloc.user!.cheptel;
+  String ? get token => this._bloc.user!.token;
 
   //Future<String> saveLamb(List<LambModel> lambs );
   // Agnelage et agneaux
   Future<String> saveLambing(LambingModel lambing );
   Future<String> saveLamb(LambModel lambing );
   Future<String> deleteLamb(int idBd);
-  Future<LambingModel> searchLambing(int idBd);
+  Future<LambingModel?> searchLambing(int idBd);
   Future<List<CompleteLambModel>> getAllLambs(String cheptel);
   Future<List<LambingModel>> getLambs(int idBete) ;
   Future<String> boucler(LambModel lamb, Bete bete);
@@ -32,14 +32,14 @@ abstract class DataProvider {
   Future<String> saveBete(Bete bete);
   Future<String> saveSortie(String date, String motif, List<Bete> lstBete);
   Future<String> saveEntree(String cheptel, String date, String motif, List<Bete> lstBete);
-  Future<Bete> getMere(Bete bete);
+  Future<Bete?> getMere(Bete bete);
   // Traitement
   Future<String> saveTraitement(TraitementModel traitement);
   Future<String> saveTraitementCollectif(TraitementModel traitement, List<Bete> betes);
   Future<String> deleteTraitement(int idBd);
   Future<List<TraitementModel>> getTraitements(Bete bete);
   Future<List<TraitementModel>> getTraitementsForLamb(LambModel lamb);
-  Future<TraitementModel> searchTraitement(int idBd);
+  Future<TraitementModel?> searchTraitement(int idBd);
   // Pesee
   Future<String> savePesee(Pesee pesee);
   Future<List<Pesee>> getPesee(Bete bete);
@@ -47,19 +47,19 @@ abstract class DataProvider {
   Future<String> deletePesee(int idBd);
   // Echographie
   Future<String> saveEcho(EchographieModel echo);
-  Future<EchographieModel> searchEcho(int idBd);
+  Future<EchographieModel?> searchEcho(int idBd);
   Future<List<EchographieModel>> getEcho(Bete bete);
   // Note etat corporel
   Future<String> saveNec(NoteModel node);
   Future<List<NoteModel>> getNec(Bete bete);
   // Lots
   Future<List<LotModel>> getLots(String cheptel) ;
-  Future<LotModel> saveLot(LotModel lot);
+  Future<LotModel?> saveLot(LotModel lot);
   Future<List<Affectation>>getBrebisForLot(int idLot);
   Future<List<Affectation>>getBeliersForLot(int idLot);
   Future<List<Affectation>>getAffectationForBete(int idBete);
   Future<List<Bete>>getBrebis();
   Future<List<Bete>>getBeliers();
-  Future<void> remove(Affectation affect);
+  Future<String> remove(Affectation affect);
   Future<String> addBete(LotModel lot, Bete bete, String dateEntree);
 }
