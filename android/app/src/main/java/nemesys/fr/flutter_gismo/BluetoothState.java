8f07b3622c01527fbb742463ba10a08df20104e7
xavier <xavier.beaufils@firesoft.fr>
2021-09-08 00:13:13 +0200

    Bluetooth

--- android/app/src/main/java/nemesys/fr/flutter_gismo/BluetoothState.java
+++ android/app/src/main/java/nemesys/fr/flutter_gismo/BluetoothState.java
@@ -9,7 +9,7 @@ public class BluetoothState {
     public static final int STATE_NULL = -1;  	 	// now service is null
 
     // Message types sent from the BluetoothChatService Handler
-    public static final int MESSAGE_STATE_CHANGE = 1;
+    //public static final int MESSAGE_STATE_CHANGE = 1;
     public static final int MESSAGE_READ = 2;
     public static final int MESSAGE_WRITE = 3;
     public static final int MESSAGE_DEVICE_NAME = 4;
