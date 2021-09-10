b13a4000d43877426090dd02719b5eaa34ddeeda
xbeau <xbeaufils@gmail.com>
2021-09-10 16:45:28 +0200

    Bluetooth

--- lib/bloc/GismoBloc.dart
+++ lib/bloc/GismoBloc.dart
@@ -109,14 +109,28 @@ class GismoBloc {
     }
   }
 
+  Future<BluetoothState> startReadBluetooth() async {
+    BluetoothState state;
+    FlutterSecureStorage storage = new FlutterSecureStorage();
+    String address = await storage.read(key: "address");
+    debug.log("read data status " + address, name: "GismoBloc::startReadBluetooth");
+    String status = await BLUETOOTH_CHANNEL.invokeMethod("readBlueTooth", { 'address': address});
+    debug.log("read status " + status, name: "GismoBloc::startReadBluetooth");
+    state = BluetoothState.fromResult(json.decode(status));
+    return state;
+  }
+
   Stream <BluetoothState> streamReadBluetooth() async* {
+    /*
     BluetoothState state;
     FlutterSecureStorage storage = new FlutterSecureStorage();
     String address = await storage.read(key: "address");
     debug.log("read data status " + address, name: "GismoBloc::streamReadBluetooth");
     String status = await BLUETOOTH_CHANNEL.invokeMethod("readBlueTooth", { 'address': address});
     debug.log("read status " + status, name: "GismoBloc::streamReadBluetooth");
-
+   */
+    String status;
+    BluetoothState state;
     int i = 0;
     while (i < 20) {
       status = await BLUETOOTH_CHANNEL.invokeMethod("dataBlueTooth");
@@ -162,6 +176,7 @@ class GismoBloc {
   void stopBluetooth() {
     BLUETOOTH_CHANNEL.invokeMethod("stopBlueTooth");
   }
+
   void stopReadBluetooth() {
     BLUETOOTH_CHANNEL.invokeMethod("stopReadBlueTooth");
   }
