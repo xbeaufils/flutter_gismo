63c37725981924b24d982e327c3c409e55707a7e
xbeau <xbeaufils@gmail.com>
2023-03-03 16:47:36 +0100

    Internationalisation

--- lib/traitement/Sanitaire.dart
+++ lib/traitement/Sanitaire.dart
@@ -3,7 +3,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gismo/bloc/GismoBloc.dart';
 import 'package:flutter_gismo/generated/l10n.dart';
-//import 'package:flutter_gismo/main.dart';
 import 'package:flutter_gismo/model/BeteModel.dart';
 import 'package:flutter_gismo/model/LambModel.dart';
 import 'package:flutter_gismo/model/TraitementModel.dart';
