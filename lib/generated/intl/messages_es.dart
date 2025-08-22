8087653d61892cbf84203f6911c4ceaac56d6a37
beaufilx <Est1tiam2y@24>
2025-08-22 14:46:45 +0200

    Espagnol

---
+++ lib/generated/intl/messages_es.dart
@@ -0,0 +1,222 @@
+// DO NOT EDIT. This is code generated via package:intl/generate_localized.dart
+// This is a library that provides messages for a es locale. All the
+// messages from the main program should be duplicated here with the same
+// function name.
+
+// Ignore issues from commonly used lints in this file.
+// ignore_for_file:unnecessary_brace_in_string_interps, unnecessary_new
+// ignore_for_file:prefer_single_quotes,comment_references, directives_ordering
+// ignore_for_file:annotate_overrides,prefer_generic_function_type_aliases
+// ignore_for_file:unused_import, file_names, avoid_escaping_inner_quotes
+// ignore_for_file:unnecessary_string_interpolations, unnecessary_string_escapes
+
+import 'package:intl/intl.dart';
+import 'package:intl/message_lookup_by_library.dart';
+
+final messages = new MessageLookup();
+
+typedef String MessageIfAbsent(String messageStr, List<dynamic> args);
+
+class MessageLookup extends MessageLookupByLibrary {
+  String get localeName => 'es';
+
+  final messages = _notInlinedMessages(_notInlinedMessages);
+  static Map<String, Function> _notInlinedMessages(_) => <String, Function>{
+    "aborted": MessageLookupByLibrary.simpleMessage("Abortado"),
+    "add_lamb": MessageLookupByLibrary.simpleMessage("Añadir un cordero"),
+    "adoption_abandon": MessageLookupByLibrary.simpleMessage(
+      "Abandona huyendo",
+    ),
+    "adoption_apathique": MessageLookupByLibrary.simpleMessage("Apática"),
+    "adoption_default_title": MessageLookupByLibrary.simpleMessage(
+      "Defecto de adopción",
+    ),
+    "adoption_delaisse": MessageLookupByLibrary.simpleMessage(
+      "Abandona a uno de los corderos",
+    ),
+    "adoption_infanticide": MessageLookupByLibrary.simpleMessage(
+      "Infanticidio",
+    ),
+    "adoption_leche": MessageLookupByLibrary.simpleMessage(
+      "Lame, llama y se deja mamar",
+    ),
+    "adoption_quality": MessageLookupByLibrary.simpleMessage(
+      "Calidad de adopción",
+    ),
+    "adoption_tenir": MessageLookupByLibrary.simpleMessage(
+      "Retiene a la fuerza a la madre",
+    ),
+    "alive": MessageLookupByLibrary.simpleMessage("Vivo"),
+    "all_ram": MessageLookupByLibrary.simpleMessage("Todas las memorias RAM"),
+    "batch": MessageLookupByLibrary.simpleMessage("Lote"),
+    "batch_campaign": MessageLookupByLibrary.simpleMessage("Campaña"),
+    "batch_date": MessageLookupByLibrary.simpleMessage("Fechas"),
+    "batch_name": MessageLookupByLibrary.simpleMessage("Nombre del lote"),
+    "batch_no_code": MessageLookupByLibrary.simpleMessage(
+      "Sin nombre del lote",
+    ),
+    "batch_no_date_debut": MessageLookupByLibrary.simpleMessage(
+      "Sin fecha de inicio",
+    ),
+    "batch_no_date_fin": MessageLookupByLibrary.simpleMessage(
+      "Sin fecha de finalización",
+    ),
+    "batch_warning": MessageLookupByLibrary.simpleMessage(
+      "Debe guardar el lote antes de añadirlo",
+    ),
+    "body_cond": MessageLookupByLibrary.simpleMessage(
+      "Puntuación de condición corporal",
+    ),
+    "body_cond_full": MessageLookupByLibrary.simpleMessage(
+      "Puntuación de condición corporal",
+    ),
+    "breastfeeding_mode": MessageLookupByLibrary.simpleMessage(
+      "Lactancia materna modo",
+    ),
+    "breastfeeding_not_specified": MessageLookupByLibrary.simpleMessage(
+      "Lactancia materna no especificada",
+    ),
+    "configuration": MessageLookupByLibrary.simpleMessage("Configuración"),
+    "data_available": MessageLookupByLibrary.simpleMessage("Datos recibidos"),
+    "dateDeparture": MessageLookupByLibrary.simpleMessage("Fecha de salida"),
+    "dateEntry": MessageLookupByLibrary.simpleMessage("Fecha de entrada"),
+    "earring": MessageLookupByLibrary.simpleMessage("Aro"),
+    "earring_search": MessageLookupByLibrary.simpleMessage("Búsqueda de aro"),
+    "echelle_connasse": MessageLookupByLibrary.simpleMessage(
+      "Escala de perras",
+    ),
+    "echelle_connasse_text": MessageLookupByLibrary.simpleMessage(
+      "Permite evaluar las cualidades maternales según los criterios de Stéphanie Maubé",
+    ),
+    "edit_lamb": MessageLookupByLibrary.simpleMessage("editar"),
+    "empty_list": MessageLookupByLibrary.simpleMessage(
+      "Lista vacía de animales",
+    ),
+    "enter_flock_number": MessageLookupByLibrary.simpleMessage(
+      "Ingrese un número de rebaño",
+    ),
+    "enter_identity_number": MessageLookupByLibrary.simpleMessage(
+      "Ingrese un número de bucle",
+    ),
+    "enter_lambing_date": MessageLookupByLibrary.simpleMessage(
+      "Introducir una fecha de parto",
+    ),
+    "entree_birth": MessageLookupByLibrary.simpleMessage("Nacimiento"),
+    "entree_creation": MessageLookupByLibrary.simpleMessage("Creación"),
+    "entree_loan": MessageLookupByLibrary.simpleMessage("Préstamo o pensión"),
+    "entree_purchase": MessageLookupByLibrary.simpleMessage("Compra"),
+    "entree_reactivation": MessageLookupByLibrary.simpleMessage("Reactivación"),
+    "entree_reason_required": MessageLookupByLibrary.simpleMessage(
+      "Motivo de entrada obligatorio",
+    ),
+    "entree_renewal": MessageLookupByLibrary.simpleMessage("Renovación"),
+    "entree_select": MessageLookupByLibrary.simpleMessage(
+      "Seleccione un motivo de entrada",
+    ),
+    "entree_transfer": MessageLookupByLibrary.simpleMessage(
+      "Transferencia interna",
+    ),
+    "entree_unknown": MessageLookupByLibrary.simpleMessage("Desconocido"),
+    "flock_number": MessageLookupByLibrary.simpleMessage("Número de rebaño"),
+    "flock_number_hint": MessageLookupByLibrary.simpleMessage("Rebaño"),
+    "flock_number_warn": MessageLookupByLibrary.simpleMessage(
+      "Falta el número de rebaño",
+    ),
+    "health": MessageLookupByLibrary.simpleMessage("Estado de salud"),
+    "herd_size": MessageLookupByLibrary.simpleMessage("Recuento de cabezas"),
+    "identity_number": MessageLookupByLibrary.simpleMessage("Número de bucle"),
+    "identity_number_error": MessageLookupByLibrary.simpleMessage(
+      "El número de bucle ya está presente",
+    ),
+    "identity_number_hint": MessageLookupByLibrary.simpleMessage("Bucle"),
+    "identity_number_warn": MessageLookupByLibrary.simpleMessage(
+      "Falta el número de bucle",
+    ),
+    "input": MessageLookupByLibrary.simpleMessage("Entrada"),
+    "lamb": MessageLookupByLibrary.simpleMessage("Cordero"),
+    "lambing": MessageLookupByLibrary.simpleMessage("Parto"),
+    "lambing_date": MessageLookupByLibrary.simpleMessage("Fecha de parto"),
+    "lambing_default": MessageLookupByLibrary.simpleMessage(
+      "Predeterminado de parto",
+    ),
+    "lambing_quality": MessageLookupByLibrary.simpleMessage("Calidad de parto"),
+    "lambs": MessageLookupByLibrary.simpleMessage("Corderos"),
+    "localuser": MessageLookupByLibrary.simpleMessage("Usuario local"),
+    "mating": MessageLookupByLibrary.simpleMessage("Apareamiento"),
+    "memo": MessageLookupByLibrary.simpleMessage("Nota"),
+    "name": MessageLookupByLibrary.simpleMessage("Nombre pequeño"),
+    "name_hint": MessageLookupByLibrary.simpleMessage("Apellido"),
+    "new_lamb": MessageLookupByLibrary.simpleMessage("Nuevo cordero"),
+    "noDateDeparture": MessageLookupByLibrary.simpleMessage(
+      "Sin fecha de salida",
+    ),
+    "noEntryDate": MessageLookupByLibrary.simpleMessage("Sin fecha de entrada"),
+    "no_earring_read": MessageLookupByLibrary.simpleMessage(
+      "No se leyó el pendiente",
+    ),
+    "no_lamb": MessageLookupByLibrary.simpleMessage("¿Parto sin cordero?"),
+    "no_ram_found": MessageLookupByLibrary.simpleMessage(
+      "No se encontró ninguna memoria RAM con los criterios seleccionados",
+    ),
+    "no_weighing_date": MessageLookupByLibrary.simpleMessage(
+      "Sin fecha de pesaje",
+    ),
+    "no_weight_entered": MessageLookupByLibrary.simpleMessage(
+      "No se ha introducido ningún peso",
+    ),
+    "not_connected": MessageLookupByLibrary.simpleMessage("No conectado"),
+    "observations": MessageLookupByLibrary.simpleMessage("Observaciones"),
+    "output": MessageLookupByLibrary.simpleMessage("Salida"),
+    "output_auto": MessageLookupByLibrary.simpleMessage("Salida automática"),
+    "output_boucherie": MessageLookupByLibrary.simpleMessage(
+      "Venta de carnicería",
+    ),
+    "output_conso": MessageLookupByLibrary.simpleMessage("Autoconsumo"),
+    "output_death": MessageLookupByLibrary.simpleMessage("Muerte"),
+    "output_error": MessageLookupByLibrary.simpleMessage("Error de número"),
+    "output_loan": MessageLookupByLibrary.simpleMessage("Préstamo o internado"),
+    "output_mutation": MessageLookupByLibrary.simpleMessage("Mutación interna"),
+    "output_reason_required": MessageLookupByLibrary.simpleMessage(
+      "Motivo de salida obligatorio",
+    ),
+    "output_select": MessageLookupByLibrary.simpleMessage(
+      "Seleccione un motivo de salida",
+    ),
+    "output_unknown": MessageLookupByLibrary.simpleMessage("Desconocido"),
+    "place_earring": MessageLookupByLibrary.simpleMessage("Colocar arete"),
+    "provisional_number": MessageLookupByLibrary.simpleMessage(
+      "Número provisional",
+    ),
+    "search": MessageLookupByLibrary.simpleMessage("Buscar"),
+    "search_ram": MessageLookupByLibrary.simpleMessage("Buscar al semental"),
+    "sex_warn": MessageLookupByLibrary.simpleMessage("Falta el sexo"),
+    "sheep": MessageLookupByLibrary.simpleMessage("Individuo"),
+    "stillborn": MessageLookupByLibrary.simpleMessage("Nacido muerto"),
+    "text_empty_list": MessageLookupByLibrary.simpleMessage(
+      "Para ingresar el recuento de cabezas, introdúzcalo desde la pantalla principal.",
+    ),
+    "text_lambing_default": MessageLookupByLibrary.simpleMessage(
+      "Permite evaluar los defectos del parto",
+    ),
+    "title_empty_list": MessageLookupByLibrary.simpleMessage("Vacío list"),
+    "title_lambing_default": MessageLookupByLibrary.simpleMessage(
+      "Defectos comunes del parto",
+    ),
+    "tooltip_add_beast": MessageLookupByLibrary.simpleMessage(
+      "Añadir un animal",
+    ),
+    "tooltip_search": MessageLookupByLibrary.simpleMessage("Buscar"),
+    "treatment": MessageLookupByLibrary.simpleMessage("Tratamiento"),
+    "ultrasound": MessageLookupByLibrary.simpleMessage("Ecografía"),
+    "user_error": MessageLookupByLibrary.simpleMessage("Error de usuario"),
+    "validate_lambing": MessageLookupByLibrary.simpleMessage("Validar parto"),
+    "weighing": MessageLookupByLibrary.simpleMessage("Pesaje"),
+    "weighing_date": MessageLookupByLibrary.simpleMessage("Fecha de pesaje"),
+    "weighing_error": MessageLookupByLibrary.simpleMessage(
+      "El peso no está en formato numérico",
+    ),
+    "weighing_hint": MessageLookupByLibrary.simpleMessage("Peso en kg"),
+    "weight": MessageLookupByLibrary.simpleMessage("Peso"),
+    "welcome": MessageLookupByLibrary.simpleMessage("Inicio"),
+  };
+}
